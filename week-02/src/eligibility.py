01c667096db4421bca7a8fdd5c4a20971de53f1f
Olga Jelusova <olga.jelusova@icloud.com>
2026-05-13 14:27:22 +0300

    feat: eligibility loop version

--- week-02/src/eligibility.py
+++ week-02/src/eligibility.py
@@ -1,16 +1,15 @@
-age = int(input("Ievadi savu vecumu: "))
+while True:
+    age = int(input("Ievadi savu vecumu (0 lai iziet): "))
 
-if age < 0:
-    print("Nepareizs vecums")
-elif age < 18:
-    print("Tu vēl nevari balsot")
-    age = int(input("Ievadi savu vecumu: "))
+    if age == 0:
+        print("Programma beidzas")
+        break
 
-if age < 0:
-    print("Nepareizs vecums")
-elif age < 18:
-    print("Tu vēl nevari balsot")
-elif age <= 120:
-    print("Tu vari balsot")
-else:
-    print("Vecums izskatās nereāls")
+    if age < 0:
+        print("Nepareizs vecums")
+    elif age < 18:
+        print("Tu vēl nevari balsot")
+    elif age <= 120:
+        print("Tu vari balsot")
+    else:
+        print("Vecums izskatās nereāls")
